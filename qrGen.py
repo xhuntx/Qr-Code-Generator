91a505f7954d3bec33549dfd2bba4ebf3bcb1adf
Hunter Davis <davishunterboy@gmail.com>
2025-08-31 11:31:54 -0400

    Added Network support

--- qrGen.py
+++ qrGen.py
@@ -1,16 +1,26 @@
 import qrcode
 import pathlib
 
+userChoice = input("What type of QR code do you need? (Link or for a Wifi netowrk?) ").lower()
+
 qr = qrcode.QRCode(
     version=1,
     error_correction=qrcode.constants.ERROR_CORRECT_L,
     box_size=10,
     border=4,
 )
-qr.add_data(input("Please input a valid link. "))
-qr.make(fit=True)
+while True:
+    if userChoice == "link":
+        qr.add_data(input("Please input a valid link. "))
+        qr.make(fit=True)
+         
+        if "https://" not in qr.add_data:
+            print("Invalid link")
+            break 
+        
+        img = qr.make_image(fill_color="black", back_color="white")
+        save = pathlib.Path.home() / "Downloads"/ "QR Code.png"
+        img.save(save)
+        print(f"Your QR code was saved in {save}")
 
-img = qr.make_image(fill_color="black", back_color="white")
-save = pathlib.Path.home() / "Downloads"/ "QR Code.png"
-img.save(save)
-print(f"Your QR code was saved in {save}")
+    
